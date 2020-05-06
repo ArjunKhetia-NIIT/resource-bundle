66114872396b15dbcc147b82e7abe486bcee1985
ArjunKhetia-NIIT <arjunj@ilimi.in>
2020-05-06 11:33:46 +0530

    distFolder and merge added

--- index.js
+++ index.js
@@ -3,23 +3,37 @@ var json = require('./formatjson');
 var phrase = require('./phrase');
 var fs = require('fs');
 var async = require('async');
-const distFolder = 'sunbirdresourcebundle';
+var flatten = require('flat');
+var unflatten = require('flat').unflatten;
 var authToken, project, locale, fileformat;
 const args = process.argv.slice(2);
 const projectURL = 'https://api.phrase.com/v2/projects';
-const encoding = 'UTF-8'; 
+const encoding = 'UTF-8';
 // Valid options are "UTF-8", "UTF-16" and "ISO-8859-1"
+var distFolder = 'sunbirdresourcebundle';
+var files = [];
+var merge = false;
 
-fs.mkdir(distFolder, { recursive: true }, (err) => {
-    if (err) throw err;
-});
+makeDistFolder = (distFolderName) => {
+    fs.mkdir(distFolderName, { recursive: true }, (err) => {
+        if (err) throw err;
+    });
+}
+
+if (_.findIndex(args, function(arg) { return arg.includes('-distFolder') }) !== -1) {
+    const value = args[_.findIndex(args, function(arg) { return arg.includes('-distFolder') })];
+    distFolder = value.slice(12);
+    makeDistFolder(distFolder);
+} else {
+    makeDistFolder(distFolder);
+}
 
 _.forEach(args, function (arg) {
     if (arg.includes('-authToken')) {
         authToken = arg.slice(11);
     }
     if (arg.includes('-project')) {
-        project = arg.slice(9);
+        project = arg.slice(9).split(",");
     }
     if (arg.includes('-locale')) {
         locale = arg.slice(8).split(",");
@@ -27,56 +41,112 @@ _.forEach(args, function (arg) {
     if (arg.includes('-fileformat')) {
         fileformat = arg.slice(12);
     }
+    if (arg.includes('-merge')) {
+        merge = arg.slice(7);
+    }
 });
 
-if (authToken && project && locale && fileformat) {
-    phrase.getProjects(authToken, projectURL)
-        .then((data) => {
-            const projects = JSON.parse(data);
-            _.forEach(projects, function(Project) {
-                if (Project['name'] === project ) {
-                    const projectId = Project['id'];
-                    phrase.getLocales(authToken, projectURL, projectId)
-                        .then((data) => {
-                            const locales = JSON.parse(data);
-                            if (locales && (locales.length > 0)) {
-                                _.forEach(locales, function(element) {
-                                    if (locale.indexOf(element['code']) !== -1) {
-                                        const localeId = element['id'];
-                                        phrase.downloadFile(authToken, projectURL, projectId, localeId, fileformat, encoding)
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
+}
+
+const run = async () => {
+    if (authToken) {
+        if (project) {
+            if (locale) {
+                if (fileformat) {
+                    await phrase.getProjects(authToken, projectURL)
+                        .then(async (data) => {
+                            const projects = await JSON.parse(data);
+                            await asyncForEach(projects, async (Project) => {
+                                await asyncForEach(project, async (prj) => {
+                                    if (Project['name'] === prj) {
+                                        const projectId = await Project['id'];
+                                        await phrase.getLocales(authToken, projectURL, projectId)
                                             .then(async (data) => {
-                                                if (fileformat === 'nested_json' || fileformat === 'json' || fileformat === 'react_simple_json'
-                                                || fileformat === 'react_nested_json' || fileformat === 'simple_json' || fileformat === 'go_i18n'
-                                                || fileformat === 'angular_translate' || fileformat === 'i18next') {
-                                                    const object = JSON.parse(data);
-                                                    await json.formatjson(object);
-                                                    fs.writeFile(distFolder + '/' + element['code']+'.json', JSON.stringify(object), encoding, () => {
-                                                        console.log(element['code']+'.json File Generated');
-                                                    });
+                                                const locales = await JSON.parse(data);
+                                                if (locales && (locales.length > 0)) {
+                                                    await asyncForEach(locales, async (element) => {
+                                                        if (locale.indexOf(element['code']) !== -1) {
+                                                            const localeId = await element['id'];
+                                                            await phrase.downloadFile(authToken, projectURL, projectId, localeId, fileformat, encoding)
+                                                                .then(async (data) => {
+                                                                    if (fileformat === 'nested_json' || fileformat === 'json' || fileformat === 'react_simple_json'
+                                                                        || fileformat === 'react_nested_json' || fileformat === 'simple_json' || fileformat === 'go_i18n'
+                                                                        || fileformat === 'angular_translate' || fileformat === 'i18next') {
+                                                                        const object = await JSON.parse(data);
+                                                                        await json.formatjson(object);
+                                                                        await fs.writeFile(distFolder + '/' + prj + ' - ' + element['code'] + '.json', JSON.stringify(object), encoding, () => {
+                                                                            files.push(prj + ' - ' + element['code'] + '.json');
+                                                                            if (merge === false) {
+                                                                                console.log(prj + ' - ' + element['code'] + '.json File Generated');
+                                                                            }
+                                                                        });
+                                                                    } else {
+                                                                        await fs.writeFile(distFolder + '/' + prj + ' - ' + element['code'] + '.' + fileformat, data, encoding, () => {
+                                                                            console.log(prj + ' - ' + element['code'] + '.' + fileformat + ' File Generated');
+                                                                        });
+                                                                    }
+                                                                })
+                                                                .catch((err) => {
+                                                                    console.log('Error ', err);
+                                                                });
+                                                        }
+                                                    })
                                                 } else {
-                                                    fs.writeFile(distFolder + '/' + element['code']+'.'+fileformat, data, encoding, () => {
-                                                        console.log(element['code']+'.'+fileformat+' File Generated');
-                                                    });
+                                                    console.log('Locales Not Found');
                                                 }
                                             })
                                             .catch((err) => {
                                                 console.log('Error ', err);
                                             });
                                     }
-                                });
-                            } else {
-                                console.log('Locales Not Found');
-                            }
+                                })
+                            })
                         })
                         .catch((err) => {
                             console.log('Error ', err);
                         });
+                    if ((fileformat === 'nested_json' || fileformat === 'json' || fileformat === 'react_simple_json'
+                        || fileformat === 'react_nested_json' || fileformat === 'simple_json' || fileformat === 'go_i18n'
+                        || fileformat === 'angular_translate' || fileformat === 'i18next') && (merge === 'true')) {
+                        setTimeout(async () => {
+                            await asyncForEach(locale, async (Locale) => {
+                                let result = [];
+                                let jsonresult = {};
+                                for (let index = 0; index < files.length; index++) {
+                                    if (files[index].includes(Locale)) {
+                                        const json = await JSON.parse(fs.readFileSync(distFolder + '/' + files[index], 'utf8'));
+                                        const flatjson = await flatten(json);
+                                        result.push(flatjson);
+                                        fs.unlinkSync(distFolder + '/' + files[index]);
+                                    }
+                                    if (index === files.length - 1) {
+                                        await asyncForEach(result, async (res) => {
+                                            await Object.assign(jsonresult, res);
+                                            const unfjson = await unflatten(jsonresult);
+                                            await fs.writeFile(distFolder + '/' + Locale.substring(0, 2) + '.json', JSON.stringify(unfjson), encoding, () => {
+                                                console.log(Locale.substring(0, 2) + '.json File Generated');
+                                            });
+                                        })
+                                    }
+                                }
+                            })
+                        }, 2000);
+                    }
+                } else {
+                    console.log('FileFormat is Missing');
                 }
-            });
-        })
-        .catch((err) => {
-            console.log('Error ', err);
-        });
-} else {
-    console.log('Some Parameters Missing');
+            } else {
+                console.log('Locale is Missing');
+            }
+        } else {
+            console.log('Project is Missing');
+        }
+    } else {
+        console.log('AuthToken is Missing');
+    }
 }
+run();
